f7a28b7460cab1b2ef1257b514d3d2a6fb239165
PerparimSH <122631933+PerparimSH@users.noreply.github.com>
2023-04-06 20:20:36 +0200

    Update Header.js

--- src/js/Header.js
+++ src/js/Header.js
@@ -1,14 +1,14 @@
-import React, { useState } from 'react';
-import { Link, NavLink, Outlet, useLocation } from 'react-router-dom';
+import React, { useState } from "react";
+import { Link, NavLink, Outlet, useLocation } from "react-router-dom";
 
 function MainHeader(props) {
-  const user = JSON.parse(localStorage.getItem('user'));
+  const user = JSON.parse(localStorage.getItem("user"));
   const location = useLocation();
 
   const handleLogout = () => {
-    localStorage.removeItem('user');
+    localStorage.removeItem("user");
     // TODO: Implement logout functionality
-  }
+  };
 
   return (
     <header className="main-header">
@@ -16,15 +16,29 @@ function MainHeader(props) {
         <img src="https://coderslabkosova.com/blocks/MainHeader/logoCLen.svg" />
       </div>
       <ul className="main-menu">
-        <li><Link to="/">Home</Link></li>
-        <li><Link to="/prices">Prices</Link></li>
-        <li><Link to="#about">About us</Link></li>
-        <li><Link to="#contact">Contact</Link></li>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/prices">Prices</Link>
+        </li>
+        <li>
+          <Link to="#about">About us</Link>
+        </li>
+        <li>
+          <Link to="#contact">Contact</Link>
+        </li>
         <li>{user && <Link to="/dashboard">My Profile</Link>}</li>
         <li>{!user && <Link to="/login">Login</Link>}</li>
-        <li>{user && <Link to="/login" onClick={handleLogout}>Logout</Link>}</li>
+        <li>
+          {user && (
+            <Link to="/login" onClick={handleLogout}>
+              Logout
+            </Link>
+          )}
+        </li>
       </ul>
-      <Outlet/>
+      <Outlet />
     </header>
   );
 }
