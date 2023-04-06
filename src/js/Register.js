9cf915a7efdac4732541588f1c625e3c027a970b
PerparimSH <122631933+PerparimSH@users.noreply.github.com>
2023-04-06 20:19:26 +0200

    Update Register.js

--- src/js/Register.js
+++ src/js/Register.js
@@ -1,14 +1,12 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 
 const Register = () => {
-
   const API = "http://localhost:3000";
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
-  const [confirmPassword, setConfirmPassword] = useState('');
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
   const [success, setSuccess] = useState(false);
   const [failure, setFailure] = useState(false);
-
 
   const handleRegister = async (e) => {
     e.preventDefault();
@@ -28,11 +26,11 @@ const Register = () => {
 
     try {
       const response = await fetch(`${API}/users`, {
-        method: 'POST',
+        method: "POST",
         headers: {
-          'Content-Type': 'application/json'
+          "Content-Type": "application/json",
         },
-        body: JSON.stringify(data)
+        body: JSON.stringify(data),
       });
 
       const responseData = await response.json();
@@ -45,26 +43,43 @@ const Register = () => {
       }, 3000); // set timeout for 3 seconds
 
       // Clear input fields after registration is successful
-      setUsername('');
-      setPassword('');
-      setConfirmPassword('');
+      setUsername("");
+      setPassword("");
+      setConfirmPassword("");
     } catch (error) {
       console.log(error);
     }
-  }
+  };
 
   return (
-    <>    
-      {success && <p className='register-message'>Registration successful</p>}
-      {failure && <p className='register-message2'>Passwords do not match</p>}
+    <>
+      {success && <p className="register-message">Registration successful</p>}
+      {failure && <p className="register-message2">Passwords do not match</p>}
 
-      <form className='loginpage' onSubmit={handleRegister}>
+      <form className="loginpage" onSubmit={handleRegister}>
         <h2>Register</h2>
-        <input type="username" placeholder="Username" value={username} onChange={(e) => setUsername(e.target.value)} />
-        <input type="password" placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)} />
-        <input type="password" placeholder="Confirm Password" value={confirmPassword} onChange={(e) => setConfirmPassword(e.target.value)} />
+        <input
+          type="username"
+          placeholder="Username"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+        />
+        <input
+          type="password"
+          placeholder="Password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <input
+          type="password"
+          placeholder="Confirm Password"
+          value={confirmPassword}
+          onChange={(e) => setConfirmPassword(e.target.value)}
+        />
         <input type="submit" value="Register"></input>
-        <p><span>Already have and account?</span> <a href='#/login'>Login</a></p>
+        <p>
+          <span>Already have and account?</span> <a href="#/login">Login</a>
+        </p>
       </form>
     </>
   );
